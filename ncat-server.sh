3ff1939450376fb62d2a9a27a69d5797d4457d5b
Bruno Bord <bruno@jehaisleprintemps.net>
2021-03-24 21:31:41 +0100

    Add help option for the server script
    refs #2

--- ncat-server.sh
+++ ncat-server.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
+help() {
+    echo "Usage:"
+    echo
+    echo "To run a server with the default parameters:"
+    echo
+    echo "   $0"
+    echo
+    echo "Use the following environment variable to change the connection parameters:"
+    echo
+    echo "'CHAT_PORT': TCP port (default: 12345)"
+    echo
+    echo "Example:"
+    echo
+    echo "   CHAT_PORT=9999 $0"
+    echo
+}
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+  help
+  exit 1
+fi
+
 CHAT_PORT="${CHAT_PORT:-12345}"
 echo "Running the server on port $CHAT_PORT"
 ncat --broker --listen -p $CHAT_PORT
